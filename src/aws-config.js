7d6b1169a382681159efdf8af3206a3f479de774
Zachary Temple <zachary.temple@agvance.net>
2020-10-22 15:14:38 -0500

    Set up initial Cognito configurations for project.

---
+++ src/aws-config.js
@@ -0,0 +1,8 @@
+const awsconfig = {
+  Auth: {
+   region: 'us-east-2',
+   userPoolId: 'us-east-2_8z5zqATf0',
+   userPoolWebClientId: '34l82pftc24l2cd8mrpjgabd48',
+  },
+ };
+ export default awsconfig;

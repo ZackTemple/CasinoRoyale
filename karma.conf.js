d072f83771032fdcec57757ca58543a7bf7277ce
Zachary Temple <zachary.temple@agvance.net>
2020-11-10 09:08:00 -0600

    Change browser for karma to chrome headless.

--- karma.conf.js
+++ karma.conf.js
@@ -25,8 +25,8 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    // browsers: ['ChromeHeadless'],
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'],
+    // browsers: ['Chrome'],
     singleRun: false,
     restartOnFileChange: true
   });
